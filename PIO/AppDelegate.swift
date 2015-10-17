30439a3e781d336abce899a2829c7113cff01802
Andrew W. Donoho <github@store.ddg.com>
2015-10-17 18:12:10 -0500

    Swiftly these former Objective-C methods.

--- PIO/AppDelegate.swift
+++ PIO/AppDelegate.swift
@@ -14,17 +14,35 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
 
     var window: UIWindow?
 
+    func configureRootViewController() {
+
+        if let svc = window?.rootViewController as? UISplitViewController {
+
+            svc.delegate = self
+
+            let controllers = svc.viewControllers
+
+            if let master = controllers.first as? UINavigationController,
+                mvc = master.topViewController as? MasterViewController {
+
+                    mvc.managedObjectContext = self.managedObjectContext
+
+                    if let detail = controllers.last as? UINavigationController,
+                        navItem = detail.topViewController?.navigationItem
+                        where detail != master {
+
+                            navItem.leftItemsSupplementBackButton = true
+                            navItem.leftBarButtonItem = svc.displayModeButtonItem()
+                    }
+            }
+        }
+        
+    } // configureRootViewController()
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
-        let splitViewController = self.window!.rootViewController as! UISplitViewController
-        let navigationController = splitViewController.viewControllers[splitViewController.viewControllers.count-1] as! UINavigationController
-        navigationController.topViewController!.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem()
-        splitViewController.delegate = self
+        configureRootViewController()
 
-        let masterNavigationController = splitViewController.viewControllers[0] as! UINavigationController
-        let controller = masterNavigationController.topViewController as! MasterViewController
-        controller.managedObjectContext = self.managedObjectContext
         return true
     }
 
@@ -55,20 +73,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
     // MARK: - Split view
 
     func splitViewController(splitViewController: UISplitViewController, collapseSecondaryViewController secondaryViewController:UIViewController, ontoPrimaryViewController primaryViewController:UIViewController) -> Bool {
-        guard let secondaryAsNavController = secondaryViewController as? UINavigationController else { return false }
-        guard let topAsDetailController = secondaryAsNavController.topViewController as? DetailViewController else { return false }
-        if topAsDetailController.detailItem == nil {
+        guard let dnc = secondaryViewController as? UINavigationController,
+            dvc = dnc.topViewController as? DetailViewController else { return false }
+
+        if dvc.detailItem == nil {
             // Return true to indicate that we have handled the collapse by doing nothing; the secondary controller will be discarded.
             return true
         }
         return false
     }
+
     // MARK: - Core Data stack
 
     lazy var applicationDocumentsDirectory: NSURL = {
         // The directory the application uses to store the Core Data store file. This code uses a directory named "com.ddg.PIO" in the application's documents Application Support directory.
         let urls = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
-        return urls[urls.count-1]
+        return urls.last!
     }()
 
     lazy var managedObjectModel: NSManagedObjectModel = {
@@ -82,20 +102,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
         // Create the coordinator and store
         let coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
         let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent("SingleViewCoreData.sqlite")
-        var failureReason = "There was an error creating or loading the application's saved data."
-        do {
-            try coordinator.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: nil)
-        } catch {
-            // Report any error we got.
-            var dict = [String: AnyObject]()
-            dict[NSLocalizedDescriptionKey] = "Failed to initialize the application's saved data"
-            dict[NSLocalizedFailureReasonErrorKey] = failureReason
 
-            dict[NSUnderlyingErrorKey] = error as NSError
-            let wrappedError = NSError(domain: "YOUR_ERROR_DOMAIN", code: 9999, userInfo: dict)
+        do { try coordinator.addPersistentStoreWithType(NSSQLiteStoreType,
+            configuration: nil,
+            URL: url,
+            options: nil)
+        }
+        catch let addError as NSError {
+
+            // Report any error we got.
+            let userInfo = [
+                NSLocalizedDescriptionKey: "Failed to initialize the application's saved data",
+                NSLocalizedFailureReasonErrorKey: "There was an error creating or loading the application's saved data.",
+                NSUnderlyingErrorKey: addError
+            ]
+            let error = NSError(domain: "com.ddg.error", code: 9999, userInfo: userInfo)
             // Replace this with code to handle the error appropriately.
             // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            NSLog("Unresolved error \(wrappedError), \(wrappedError.userInfo)")
+            NSLog("Unresolved error \(error), \(error.userInfo)")
             abort()
         }
         
@@ -104,23 +128,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
 
     lazy var managedObjectContext: NSManagedObjectContext = {
         // Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.) This property is optional since there are legitimate error conditions that could cause the creation of the context to fail.
-        let coordinator = self.persistentStoreCoordinator
-        var managedObjectContext = NSManagedObjectContext(concurrencyType: .MainQueueConcurrencyType)
-        managedObjectContext.persistentStoreCoordinator = coordinator
-        return managedObjectContext
+        let moc = NSManagedObjectContext(concurrencyType: .MainQueueConcurrencyType)
+
+        moc.persistentStoreCoordinator = self.persistentStoreCoordinator
+
+        return moc
     }()
 
     // MARK: - Core Data Saving support
 
     func saveContext () {
         if managedObjectContext.hasChanges {
-            do {
-                try managedObjectContext.save()
-            } catch {
+            do { try managedObjectContext.save() }
+            catch let error as NSError {
                 // Replace this implementation with code to handle the error appropriately.
                 // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nserror = error as NSError
-                NSLog("Unresolved error \(nserror), \(nserror.userInfo)")
+                NSLog("Unresolved error \(error), \(error.userInfo)")
                 abort()
             }
         }
